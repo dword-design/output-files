b5a51ec0590128d04300c681f59466f4801df0b2
dword <sebastianlandw@gmail.com>
2020-02-06 16:26:15 +0000

    remove expect

--- test/files.test.js
+++ test/files.test.js
@@ -1,7 +1,6 @@
 import outputFiles from 'output-files'
 import withLocalTmpDir from 'with-local-tmp-dir'
 import glob from 'glob-promise'
-import expect from 'expect'
 
 export default () => withLocalTmpDir(async () => {
   await outputFiles({ 'foo.txt': 'foo' })
