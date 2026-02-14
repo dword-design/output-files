47a0b78ea83aba76611d723c54554faa41be7b00
Sebastian Landwehr <info@sebastianlandwehr.com>
2026-02-14 22:19:06 +0100

    fix: foo

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,16 @@ const outputFiles = async (...args: [string, Files] | [Files]) => {
   const path = (typeof args[0] === 'string' ? args[0] : '') || '.';
   const files = (typeof args[0] === 'string' ? args[1] : args[0]) || {};
   await fs.ensureDir(path);
-  return Promise.all(
-    Object.entries(files).map(([localPath, child]) => {
-      const absPath = pathLib.join(path, localPath);
-      return (typeof child === 'string' ? fs.outputFile : outputFiles)(
-        absPath,
-        child,
-      );
-    }),
+
+  await Promise.all(
+    Object.entries(files).map(
+      ([localPath, child]: [string, string | Files]) => {
+        const absPath = pathLib.join(path, localPath);
+        return typeof child === 'string'
+          ? fs.outputFile(absPath, child)
+          : outputFiles(absPath, child);
+      },
+    ),
   );
 };
 
